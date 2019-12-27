a55aed10af16751f39fd13dab4ca4199c95242da
simonkir <simon.kugelblitz@gmail.com>
2019-12-27 12:12:43 +0100

    Added multi-monitor capabilities to launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
 
 # Launch polybar
-polybar main &
+if type "xrandr"; then # multiple monitors
+
+    # launch polybar on every connected monitor
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar -r main &
+    done
+
+else # only one monitor
+    polybar -r main &
+fi
