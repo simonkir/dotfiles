29a00a0180a9c476e36a18e0f91c856e0a8a0e6b
simonkir <simon.kugelblitz@gmail.com>
2019-12-29 09:29:15 +0100

    Fixed bug where systray didn't show on main bar No. 2

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,9 +4,9 @@
 if type "xrandr"; then # multiple monitors
 
     # launch polybar on both monitors
-    MONITOR=HDMI1 polybar -r main &
-    MONITOR=eDP1  polybar -r main &
+    MONITOR=HDMI1 polybar -r main-incl-tray &
+    MONITOR=eDP1  polybar -r main-no-tray &
 
 else # only one monitor
-    polybar -r main &
+    polybar -r main-incl-tray &
 fi
