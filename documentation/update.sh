8f51f96b0c52b1eeabca6e92a4400a00aa761ba5
simonkir <simon.kugelblitz@gmail.com>
2020-05-27 08:09:12 +0200

    added update.sh

---
+++ documentation/update.sh
@@ -0,0 +1,8 @@
+# compile i3 config
+~/.i3/create_config
+
+# add execute permission to scripts
+chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.config/polybar/stop.sh
+chmod +x ~/.config/vifm/scripts/vifmimg
+chmod +x ~/.config/vifm/scripts/vifmrun
