ac51a9961957f4276e11e16ada94c07b3aad957e
simonkir <simon.kugelblitz@gmail.com>
2020-05-27 08:12:24 +0200

    update.sh: added .i3/create_config to execute permissions

--- documentation/update.sh
+++ documentation/update.sh
@@ -1,8 +1,9 @@
-# compile i3 config
-~/.i3/create_config
-
 # add execute permission to scripts
+chmod +x ~/.i3/create_config
 chmod +x ~/.config/polybar/launch.sh
 chmod +x ~/.config/polybar/stop.sh
 chmod +x ~/.config/vifm/scripts/vifmimg
 chmod +x ~/.config/vifm/scripts/vifmrun
+
+# compile i3 config
+~/.i3/create_config
