5234b80a590670096fb7fd1731f43a4e4da4ce04
simonkir <simon.kirschenmann@gmail.com>
2020-09-04 17:58:25 +0200

    doc: changed 'chmod +x' file list

--- documentation/update.sh
+++ documentation/update.sh
@@ -1,7 +1,7 @@
 # add execute permission to scripts
 chmod +x ~/.i3/create_config
 chmod +x ~/.config/polybar/launch.sh
-chmod +x ~/.config/polybar/stop.sh
+chmod +x ~/.config/polybar/scripts/tempcores.sh
 chmod +x ~/.config/vifm/scripts/vifmimg
 chmod +x ~/.config/vifm/scripts/vifmrun
 chmod +x ~/.config/spectrwm/scripts/bar.sh
