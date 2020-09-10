7fdfbf8d4807759ed793de27dc8161415e8e2a59
simonkir <simon.kirschenmann@gmail.com>
2020-09-10 19:42:38 +0200

    fish: initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+export EDITOR='nvim'
+
+# aliases
+alias ll='ls -lh'
+alias lla='ls -alh'
+alias mv='mv -i'
+alias rm='rm -I'
+alias vifm='~/.config/vifm/scripts/vifmrun'
+alias git-dtf='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+neofetch
+
+# vim:ft=bash
