63a9fe37aafba5914f0cfbdd54010ec061928086
simonkir <simon.kirschenmann@gmail.com>
2020-11-25 13:49:41 +0100

    fish: added doom-emacs alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ alias mv='mv -i'
 alias rm='rm -I'
 alias vifm='~/.config/vifm/scripts/vifmrun'
 alias git-dtf='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias doom='~/.emacs.d/bin/doom'
 
 alias ...='cd ../../'
 alias ....='cd ../../../'
