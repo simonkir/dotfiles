e4706da8df5765365598a809e4dd9cbdbfef6ba4
simonkir <simon.kirschenmann@gmail.com>
2020-11-29 09:30:35 +0100

    emacs: removed customize-section

--- .emacs.d-own/init.el
+++ .emacs.d-own/init.el
@@ -8,21 +8,4 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d-own/conf.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(atom-one-dark))
- '(custom-safe-themes
-   '("9e39a8334e0e476157bfdb8e42e1cea43fad02c9ec7c0dbd5498cf02b9adeaf1" default))
- '(package-selected-packages
-   '()))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "ADBO" :family "Source Code Pro")))))
+(org-babel-load-file (expand-file-name "~/.emacs.d/conf.org"))
