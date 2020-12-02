133e6579622dbda029bbbc8006d16f041052da29
simonkir <simon.kirschenmann@gmail.com>
2020-12-02 08:38:15 +0100

    doc: restructured update.sh

--- documentation/update.sh
+++ documentation/update.sh
@@ -7,6 +7,7 @@ chmod +x ~/.config/vifm/scripts/vifmimg
 chmod +x ~/.config/vifm/scripts/vifmrun
 chmod +x ~/.config/spectrwm/scripts/bar.sh
 chmod +x ~/.config/herbstluftwm/scripts/picom-launch.sh
+chmod +x ~/.screenlayout/default.sh
 
 # hlwm config permissions
 chmod +x ~/.config/herbstluftwm/autostart
@@ -17,7 +18,3 @@ chmod +x ~/.config/herbstluftwm/autostart.d/autostart-always
 chmod +x ~/.config/herbstluftwm/autostart.d/attributes
 chmod +x ~/.config/herbstluftwm/autostart.d/monitor-settings
 
-# compile i3 config
-~/.i3/create_config
-
-chmod +x ~/.screenlayout/default.sh
