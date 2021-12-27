28ad1a17ce9391a2e1c430f15b5af77383bbcd43
simonkir <simon.kugelblitz@gmail.com>
2021-12-27 22:38:39 +0100

    emacs / auctex: new matrix keybinds

--- .emacs.d/config.d/3-3---auctex.el
+++ .emacs.d/config.d/3-3---auctex.el
@@ -30,7 +30,7 @@
   (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
   (add-hook 'LaTeX-mode-hook 'prettify-symbols-mode)
 
-  (defun sk:activate-tex-alignment-keybinds ()
+  (defun sk:activate-tex-alignment-keybinds-equality ()
     (interactive)
     (general-def 'insert TeX-mode-map
       "C-<" '(lambda () (interactive) (insert "& \\leq &"))
@@ -38,10 +38,9 @@
       "=" '(lambda () (interactive) (insert "& = &"))
       "<" '(lambda () (interactive) (insert "& < &"))
       ">" '(lambda () (interactive) (insert "& > &")))
-      (message "Enabled TeX Alignment Keybinds")
-      (setq sk:tex-alignment-keybinds-active t))
+    (message "Enabled TeX Alignment Keybinds for Equality Operators"))
 
-  (defun sk:deactivate-tex-alignment-keybinds ()
+  (defun sk:deactivate-tex-alignment-keybinds-equality ()
     (interactive)
     (general-def 'insert TeX-mode-map
       "C-<" '(lambda () (interactive) (insert "\\leq"))
@@ -49,22 +48,41 @@
       "=" '(lambda () (interactive) (insert "="))
       "<" '(lambda () (interactive) (insert "<"))
       ">" '(lambda () (interactive) (insert ">")))
-      (message "Disabled TeX Alignment Keybinds")
-      (setq sk:tex-alignment-keybinds-active nil))
+    (message "Disabled TeX Alignment Keybinds for Equality Operators"))
 
-  (defun sk:toggle-tex-alignment-keybinds ()
+  (defun sk:activate-tex-alignment-keybinds-matrix ()
     (interactive)
-    (if sk:tex-alignment-keybinds-active
-	(sk:deactivate-tex-alignment-keybinds)
-      (sk:activate-tex-alignment-keybinds)))
+    (general-def 'insert TeX-mode-map
+      "SPC" '(lambda () (interactive) (insert " & "))
+      "C-SPC" '(lambda () (interactive) (insert " ")))
+    (message "Enabled TeX Alignment Keybinds for Matricies"))
 
-  (sk:activate-tex-alignment-keybinds)
+  (defun sk:deactivate-tex-alignment-keybinds-matrix ()
+    (interactive)
+    (general-def 'insert TeX-mode-map
+      "SPC" '(lambda () (interactive) (insert " "))
+      "C-SPC" '(lambda () (interactive) (insert " ")))
+    (message "Disabled TeX Alignment Keybinds for Matricies"))
+  
+  (defun sk:deactivate-tex-alignment-keybinds-all ()
+    (interactive)
+    (sk:deactivate-tex-alignment-keybinds-equality)
+    (sk:deactivate-tex-alignment-keybinds-matrix)
+    (message "Disabled All TeX Alignment Keybinds"))
+
+  (sk:activate-tex-alignment-keybinds-equality)
+
+  (general-def 'normal TeX-mode-map :prefix "SPC SPC k"
+    "e" 'sk:activate-tex-alignment-keybinds-equality
+    "E" 'sk:deactivate-tex-alignment-keybinds-equality
+    "m" 'sk:activate-tex-alignment-keybinds-matrix
+    "M" 'sk:deactivate-tex-alignment-keybinds-matrix
+    "K" 'sk:deactivate-tex-alignment-keybinds-all)
 
   (general-def 'insert TeX-mode-map
     "<C-return>" '(lambda () (interactive) (insert " \\\\\n  ")))
 
   (general-def 'normal TeX-mode-map :prefix "SPC SPC"
-    "c"   'sk:toggle-tex-alignment-keybinds
     "s"   'LaTeX-section            ;; insert section
     "e"   'LaTeX-environment        ;; insert environment
     "TAB" 'LaTeX-fill-environment   ;; auto-indent
