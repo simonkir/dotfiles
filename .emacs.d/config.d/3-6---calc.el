f4ad366b326a8d1290128a7840037c49bfb3b9d0
simonkir <simon.kugelblitz@gmail.com>
2022-01-30 18:27:24 +0100

    emacs: fixed quick-calc error message on first run

--- .emacs.d/config.d/3-6---calc.el
+++ .emacs.d/config.d/3-6---calc.el
@@ -10,7 +10,7 @@
 
   :config
   (setq calc-multiplication-has-precedence nil)
-  (add-hook 'calc-mode-hook 'calc-algebraic-mode)
+  ;;(add-hook 'calc-mode-hook 'calc-algebraic-mode)
 
   (general-def 'normal calc-mode-map "SPC" nil) ;; unbind SPC for the following to work
   (general-def 'normal calc-mode-map :prefix "SPC SPC"
