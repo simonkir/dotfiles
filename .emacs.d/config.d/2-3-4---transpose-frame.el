2413b24044a9f158e107e26c3c27f058db772e8b
simonkir <simon.kugelblitz@gmail.com>
2022-02-02 19:43:30 +0100

    emacs / transpose: more keybinds

--- .emacs.d/config.d/2-3-4---transpose-frame.el
+++ .emacs.d/config.d/2-3-4---transpose-frame.el
@@ -7,5 +7,7 @@
   :defer t
   :general ('normal 'override :prefix "SPC w"
 	"w" 'transpose-frame
+	"r" 'rotate-frame-clockwise
+	"R" 'rotate-frame-anticlockwise
 	"F" 'flip-frame
 	"f" 'flop-frame))
