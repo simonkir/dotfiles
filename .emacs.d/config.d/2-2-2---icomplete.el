2835ff01f11a088c763aa3e1f82d61f40280901d
simonkir <simon.kugelblitz@gmail.com>
2022-02-22 18:50:14 +0100

    emacs / icomplete: customization, included fido-mode in use-package

---
+++ .emacs.d/config.d/2-2-2---icomplete.el
@@ -0,0 +1,31 @@
+;;; -*- lexical-binding: t; -*-
+
+
+
+(use-package icomplete
+  :demand t
+  :config
+  ;; needed because icomplete--fido-mode-setup overwrites default settings
+  (add-hook 'icomplete-minibuffer-setup-hook
+            (lambda () (setq-local completion-styles '(basic partial-completion substring initials flex))))
+
+  (setq read-file-name-completion-ignore-case t)
+  (setq read-buffer-completion-ignore-case t)
+  (setq completion-ignore-case t)
+  (setq icomplete-compute-delay 0)
+
+  (general-def icomplete-minibuffer-map
+    "C-j" 'icomplete-forward-completions
+    "C-k" 'icomplete-backward-completions)
+
+  (general-def 'normal 'override
+    "SPC x" 'execute-extended-command)
+
+  (fido-mode))
+
+
+
+(use-package icomplete-vertical
+  :ensure t
+  :after icomplete
+  :config (icomplete-vertical-mode))
