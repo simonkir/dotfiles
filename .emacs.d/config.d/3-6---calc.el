340b8b3fd55b7c13fba6e5cfea52ced82354c739
simonkir <simon.kugelblitz@gmail.com>
2022-02-23 22:17:21 +0100

    emacs / calc: hotfix, emptied calc-mode-hook

--- .emacs.d/config.d/3-6---calc.el
+++ .emacs.d/config.d/3-6---calc.el
@@ -10,8 +10,6 @@
 
   :config
   (setq calc-multiplication-has-precedence nil)
-  (add-hook 'calc-mode-hook 'calc-degrees-mode)
-  ;;(add-hook 'calc-mode-hook 'calc-algebraic-mode)
 
   (general-def 'normal calc-mode-map "SPC" nil) ;; unbind SPC for the following to work
   (general-def 'normal calc-mode-map :prefix "SPC SPC"
