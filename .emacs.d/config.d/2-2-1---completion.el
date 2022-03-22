d4d11ae3c394955397995d825c57804a8ffe942c
simonkir <simon.kugelblitz@gmail.com>
2022-03-22 19:32:16 +0100

    emacs / hippie: now compatible with org-tables & yasnippet

--- .emacs.d/config.d/2-2-1---completion.el
+++ .emacs.d/config.d/2-2-1---completion.el
@@ -7,15 +7,19 @@
   :config
   (setq tab-always-indent t)
 
+  (add-to-list 'hippie-expand-try-functions-list 'yas-hippie-try-expand)
+
   (defun sk:hippie-unexpand ()
     (interactive)
     (hippie-expand 0))
 
   (defun sk:insert-tab-key ()
     (interactive)
-    (if (string-match-p "[[:alnum:]]" (char-to-string (preceding-char)))
-        (call-interactively 'hippie-expand)
-      (call-interactively 'indent-for-tab-command)))
+    (cond ((and (derived-mode-p 'org-mode) (member (nth 0 (org-element-at-point)) '(table-row table)))
+           (call-interactively 'org-table-next-field))
+          ((string-match-p "[[:alnum:]]" (char-to-string (preceding-char)))
+           (call-interactively 'hippie-expand))
+          (t (call-interactively 'indent-for-tab-command))))
 
   (general-def 'insert 'override
     "<backtab>" 'sk:hippie-unexpand
