f9880c53acfda173023490525876daeca07e2ac2
simonkir <simon.kugelblitz@gmail.com>
2022-05-08 17:43:29 +0200

    emacs / fonts: changed default fonts, added funs for changing fonts

--- .emacs.d/config.d/4-2---fonts.el
+++ .emacs.d/config.d/4-2---fonts.el
@@ -4,17 +4,48 @@
 
 ; font settings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;(set-face-attribute 'default nil :family "Fira Code" :height 100)
-;;(set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 100)
-(set-face-attribute 'default nil :family "Source Code Pro" :height 100)
-(set-face-attribute 'fixed-pitch nil :family "Source Code Pro")
-;;(set-face-attribute 'variable-pitch nil :family "Ubuntu")
-(set-face-attribute 'variable-pitch nil :family "Noto Serif")
+(set-face-attribute 'default nil :family "Noto Sans Mono" :height 100)
+(set-face-attribute 'variable-pitch nil :family "Noto Serif" :height 100)
 
 
+
+; font switching ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq sk:fonts-fixed-pitch '("Noto Sans Mono" "Inconsolata" "Roboto Mono" "Source Code Pro"))
+(setq sk:fonts-variable-pitch '("Noto Serif" "C059" "Georgia" "Ubuntu" "Roboto"))
+
+(defun sk:set-face-fixed ()
+  (interactive)
+  (let (new-face height)
+    (setq new-face (completing-read "new fixed-pitch face: " sk:fonts-fixed-pitch nil nil))
+    (if (string= new-face "Inconsolata")
+        (setq height 110)
+      (setq height 100))
+    (set-face-attribute 'default nil :family new-face :height height)))
+
+(defun sk:set-face-variable ()
+  (interactive)
+  (let (new-face height)
+    (setq new-face (completing-read "new variable-pitch face: " sk:fonts-variable-pitch nil nil))
+    (setq height 100)
+    (set-face-attribute 'variable-pitch nil :family new-face :height height))
+  (if (eq mixed-pitch-mode t)
+      (progn
+        (mixed-pitch-mode 'toggle)
+        (mixed-pitch-mode 'toggle))))
+
+(general-def '(normal visual) 'override :prefix "SPC d"
+  "f" 'sk:set-face-fixed
+  "v" 'sk:set-face-variable)
+
+
+
+; mixed-pitch mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package mixed-pitch
   :ensure t
   :hook ((org-mode TeX-mode) . mixed-pitch-mode)
-  :general ('normal 'override "SPC t m" 'mixed-pitch-mode)
-  :config (setq mixed-pitch-variable-pitch-cursor nil)) ;; keep filled cursor
+  :general ('normal 'override "SPC d m" 'mixed-pitch-mode)
+  :config
+  (setq mixed-pitch-variable-pitch-cursor nil)
+  (setq mixed-pitch-set-height t)) ;; keep filled cursor
