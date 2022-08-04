ce0226d336cf30b7d2d99e75874921cbc9d4535f
simonkir <simon.kugelblitz@gmail.com>
2022-08-04 02:18:33 +0200

    qtile: removed run function from autostart scripts

--- .config/qtile/scripts/autostart-always.sh
+++ .config/qtile/scripts/autostart-always.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-function run {
-  if ! pgrep $1 ;
-  then
-    $@&
-  fi
-}
-
-
-
 numlockx on &
 setxkbmap -option caps:escape &
--- .config/qtile/scripts/autostart-once.sh
+++ .config/qtile/scripts/autostart-once.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-function run {
-  if ! pgrep $1 ;
-  then
-    $@&
-  fi
-}
-
-
-
-#start sxhkd to replace Qtile native key-bindings
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
 
-# starting utility applications at boot time
-# run variety &
-run volumeicon &
-run nm-applet &
-# blueberry-tray &
+volumeicon &
+nm-applet &
 
-~/.config/picom/picom.sh &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
-run xfce4-power-manager &
+xfce4-power-manager &
 
-#starting user applications at boot time
-nitrogen --restore &
+~/.config/picom/picom.sh &
 ~/.emacs.d/launch-daemon.sh &
+nitrogen --restore &
