2985eeb07d56c831cde491565380c385945ceafc
simonkir <simon.kugelblitz@gmail.com>
2022-09-29 18:15:26 +0200

    emacs: new quitting functions

--- .emacs.d/config.d/2---navigation.el
+++ .emacs.d/config.d/2---navigation.el
@@ -15,14 +15,33 @@
 
 
 
-; misc ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; quitting ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun sk:save-buffers-kill-buffers-kill-terminal ()
+  (interactive)
+  (save-some-buffers)
+  (let ((buffers (mapcar 'buffer-name (buffer-list))))
+    (dolist (element buffers)
+      (if (not (or (string-match-p "^\*+" element)
+                   (string-match-p "^\s-*" element)
+                   (string-match-p "^magit" element)))
+          (kill-buffer element))))
+  (save-buffers-kill-terminal))
 
 (general-def '(normal visual) 'override :prefix "SPC"
   "ESC" 'keyboard-escape-quit
-  "q"   'save-buffers-kill-terminal
-  "Q"   'save-buffers-kill-emacs)
+  "q"   'save-buffers-kill-terminal)
 
-; help
+(general-def '(normal visual) 'override :prefix "SPC Q"
+  "q" 'save-buffers-kill-terminal
+  "Q" 'sk:save-buffers-kill-buffers-kill-terminal
+  "k" 'save-buffers-kill-emacs
+  "K" 'kill-emacs)
+
+
+
+; help ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (general-def '(normal visual) 'override :prefix "SPC h"
   "p" 'describe-package
   "f" 'describe-function
