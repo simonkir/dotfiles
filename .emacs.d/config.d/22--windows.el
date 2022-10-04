158f93e444d646240efd685533adf504d5f67773
simonkir <simon.kugelblitz@gmail.com>
2022-10-04 23:25:26 +0200

    emacs: added own window resizing function (similar to text-scale)

--- .emacs.d/config.d/22--windows.el
+++ .emacs.d/config.d/22--windows.el
@@ -2,6 +2,21 @@
 
 
 
+(defun sk:resize-current-window ()
+  (interactive)
+  (cond
+   ((eq last-command-event 'right) (call-interactively 'enlarge-window-horizontally))
+   ((eq last-command-event 'left) (call-interactively 'shrink-window-horizontally))
+   ((eq last-command-event 'up) (call-interactively 'shrink-window))
+   ((eq last-command-event 'down) (call-interactively 'enlarge-window))
+   ((eq last-command-event ?0) (call-interactively 'balance-windows)))
+  (message "Use <left>, <right>, <up>, <down>, 0 for further adjustment")
+  (set-transient-map
+   (let ((map (make-sparse-keymap)))
+     (dolist (key '(left right up down ?0))
+       (define-key map (vector key) (lambda () (interactive) (sk:resize-current-window))))
+     map)))
+
 (defun sk:split-and-follow-horizontally ()
   (interactive)
   (split-window-below)
@@ -17,11 +32,16 @@
 
 
 (general-def '(normal visual) 'override
-  "SPC w =" 'balance-windows
   "SPC w o" 'delete-other-windows
   "SPC w 1" 'delete-other-windows
   "SPC w s" 'sk:split-and-follow-horizontally
   "SPC w v" 'sk:split-and-follow-vertically
+
+  "SPC w 0"       'sk:resize-current-window
+  "SPC w <right>" 'sk:resize-current-window
+  "SPC w <left>"  'sk:resize-current-window
+  "SPC w <up>"    'sk:resize-current-window
+  "SPC w <down>"  'sk:resize-current-window
  
   "SPC w h" 'evil-window-left
   "SPC w j" 'evil-window-down
