fec79a487e3f061e3300f861ec76809fd75ead10
simonkir <simon.kugelblitz@gmail.com>
2023-04-25 02:04:51 +0200

    emacs / dired: added some keybinds

--- .emacs.d/config.d/25--dired.el
+++ .emacs.d/config.d/25--dired.el
@@ -6,11 +6,14 @@
   :config
   (setq dired-dwim-target t) ;; when two windows are next to each other, move / copy files between them
   (setq dired-kill-when-opening-new-dired-buffer t)
+  (setq dired-listing-switches "-alh")
 
   (general-def 'dired-mode-map
     "i" 'dired-toggle-read-only
     "h" 'dired-up-directory
-    "l" 'dired-find-file))
+    "l" 'dired-find-file
+    "r" 'dired-flag-garbage-files
+    ")" 'dired-omit-mode))
 
 
 
