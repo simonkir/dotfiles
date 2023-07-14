19dbfff3097ab0c95f1c9937c7e8da3225928fbb
simonkir <simon.kugelblitz@gmail.com>
2023-07-14 14:31:36 +0200

    conky: updates to launch script

--- .config/conky/launch-conky.sh
+++ .config/conky/launch-conky.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 connected_monitors=$(( $(xrandr --listactivemonitors | wc -l) - 1 ))
-connected_monitors=1
 
 killall conky
 
@@ -11,6 +10,6 @@ if [[ $connected_monitors == 2 ]]; then
     conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bl -x 40 &
     conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bl -x -1880 &
 elif [[ $connected_monitors == 1 ]]; then
-    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tl -x 40 &
+    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tl -x 80 &
     conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bl -x 40 &
 fi
