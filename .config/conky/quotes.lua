92a1c2332d0197007981c43fb7d76041f40df570
simonkir <simon.kugelblitz@gmail.com>
2023-07-15 16:36:05 +0200

    conky / quotes: better rng and some fixes

--- .config/conky/quotes.lua
+++ .config/conky/quotes.lua
@@ -1,17 +1,17 @@
 --[[
-`quotes.csv' should be located within ~/.config/conky
-    it should be formated as "AUTHOR","QUOTE"
-    the AUTHOR field can be left blank
+    `quotes.csv' contains quotes in the format of "AUTHOR","QUOTE"
+    the file should be located in ~/.config/conky/
+    the AUTHOR field can be left blank, i. e. ""
 ]]--
 
--- Load the CSV file into a table
+-- load the csv file into a table
 function loadCSV(filename)
     local file = io.open(filename, "r")
     local lines = {}
 
     for line in file:lines() do
         local fields = {}
-        for field in line:gmatch('"([^"]+)"') do
+        for field in line:gmatch('"([^"]*)"') do
             table.insert(fields, field)
         end
         table.insert(lines, fields)
@@ -21,28 +21,33 @@ function loadCSV(filename)
     return lines
 end
 
--- Choose a random line from the CSV file and return both fields as a string
-function getRandomLineFromCSV(filename)
+-- choose a random line from the csv file and return a finished quote string
+function getRandomQuoteFromCSV(filename)
     local lines = loadCSV(filename)
 
-    if #lines > 0 then
-        -- generate the same quotes on both screens
-        -- math.randomseed(os.date("%d") * os.date("%H") * os.date("%M"))
-        local randomIndex = math.random(#lines)
-        local fields = lines[randomIndex]
-        local field1 = fields[1]
-        local field2 = fields[2]
+    -- calculate a set seed for rng
+    --   – uses process id, date of month, month, year and hour to compute the seed
+    --   – with this setup, multi-monitor compatibility is guaranteed by using PID
+    local stat_file = io.open("/proc/self/stat")
+    local stat_file_contents = stat_file:read("*a")
+    stat_file:close()
+    pid = stat_file_contents:match("(%d+)")
+    math.randomseed(pid * os.date("%d%H") * os.date("%Y%m"))
 
-        if not field1 then
-            field1 = ""
-        end
+    -- read random quote
+    local randomIndex = math.random(#lines)
+    local fields = lines[randomIndex]
+    local field1 = fields[1]
+    local field2 = fields[2]
 
-        return field1:upper() .. "\n„" .. field2 .. "“"
-    else
-        return nil
+    -- formatting the result
+    if field1 == "" then
+        field1 = "Unknown Author"
     end
+
+    return field1:upper() .. "\n„" .. field2 .. "“"
 end
 
 function conky_quote()
-   return getRandomLineFromCSV(os.getenv("HOME") .. "/.config/conky/quotes.csv")
+    return getRandomQuoteFromCSV(os.getenv("HOME") .. "/.config/conky/quotes.csv")
 end
