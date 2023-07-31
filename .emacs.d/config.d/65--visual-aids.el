bb5600514b3f358fba7dd47b9b21aab2a24b6f4e
simonkir <simon.kugelblitz@gmail.com>
2023-08-01 00:35:55 +0200

    emacs / indent-guides: changed to line-bitmap

--- .emacs.d/config.d/65--visual-aids.el
+++ .emacs.d/config.d/65--visual-aids.el
@@ -27,4 +27,17 @@
 (use-package highlight-indent-guides
   :hook ((prog-mode org-mode LaTeX-mode) . highlight-indent-guides-mode)
   :general (general-def-leader "t h" 'highlight-indent-guides-mode)
-  :config (setq highlight-indent-guides-method 'bitmap))
+  :config
+  (defun sk:highlight-indent-guides--bitmap-line (width height crep zrep)
+    "Defines a solid guide line, two pixels wide.
+Use WIDTH, HEIGHT, CREP, and ZREP as described in
+`highlight-indent-guides-bitmap-function'."
+    (let* ((left (/ (- width 1) 2))
+           (right (- width left 1))
+           (row (append (make-list left zrep) (make-list 1 crep) (make-list right zrep)))
+           rows)
+      (dotimes (i height rows)
+        (setq rows (cons row rows)))))
+
+  (setq highlight-indent-guides-bitmap-function 'sk:highlight-indent-guides--bitmap-line)
+  (setq highlight-indent-guides-method 'bitmap))
