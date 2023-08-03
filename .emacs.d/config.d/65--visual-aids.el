86fcd5439fe2ac5401bc70964c3fbe9bb9267813
simonkir <simon.kugelblitz@gmail.com>
2023-08-03 12:13:56 +0200

    emacs / indent-guides: better config, impr bitmap function

--- .emacs.d/config.d/65--visual-aids.el
+++ .emacs.d/config.d/65--visual-aids.el
@@ -28,16 +28,26 @@
   :hook ((prog-mode org-mode LaTeX-mode) . highlight-indent-guides-mode)
   :general (general-def-leader "t h" 'highlight-indent-guides-mode)
   :config
-  (defun sk:highlight-indent-guides--bitmap-line (width height crep zrep)
+  ;; bitmap settings
+  (defun sk:highlight-indent-guides--bitmap-line (character-width character-height crep zrep)
     "Defines a solid guide line, two pixels wide.
 Use WIDTH, HEIGHT, CREP, and ZREP as described in
 `highlight-indent-guides-bitmap-function'."
-    (let* ((left (/ (- width 1) 2))
-           (right (- width left 1))
-           (row (append (make-list left zrep) (make-list 1 crep) (make-list right zrep)))
+    (let* ((line-width 1)
+           (padding-left (/ (- character-width line-width) 2))
+           (padding-right (- character-width padding-left line-width))
+           (row (append (make-list padding-left zrep) (make-list line-width crep) (make-list padding-right zrep)))
            rows)
-      (dotimes (i height rows)
+      (dotimes (i character-height rows)
         (setq rows (cons row rows)))))
 
   (setq highlight-indent-guides-bitmap-function 'sk:highlight-indent-guides--bitmap-line)
-  (setq highlight-indent-guides-method 'bitmap))
+  (setq highlight-indent-guides-method 'bitmap)
+
+  ;; face settings
+  (setq highlight-indent-guides-auto-character-face-perc 25)
+  (setq highlight-indent-guides-auto-top-character-face-perc 60)
+
+  ;; responsive setting
+  (setq highlight-indent-guides-responsive 'top)
+  (setq highlight-indent-guides-delay 0))
