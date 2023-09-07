fbfe2314ed8c58023cd3d52ae52aae86e1fdb698
simonkir <simon.kugelblitz@gmail.com>
2023-09-07 18:02:34 +0200

    emacs: removed auto-insert (in favor of yasnippet)

--- .emacs.d/config.d/44--snippets-templates.el
+++ .emacs.d/config.d/44--snippets-templates.el
@@ -2,25 +2,11 @@
 
 
 
-; snippets ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (use-package yasnippet
   :demand t
   :config
   (general-def yas-minor-mode-map
     "<tab>" nil
     "TAB" nil)
-  
+
   (yas-global-mode))
-
-
-
-; templates ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package autoinsert
-  :demand t
-  :config
-  (setq auto-insert nil)
-  (setq auto-insert-directory "~/.emacs.d/templates/")
-  (define-auto-insert 'org-mode "org-mode.org")
-  (define-auto-insert 'glsl-mode "glsl-mode.frag"))
