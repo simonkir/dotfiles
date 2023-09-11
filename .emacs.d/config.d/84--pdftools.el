91ae9f2b4f907a45b7a2d51988f3b938afb3112c
simonkir <simon.kugelblitz@gmail.com>
2023-09-11 15:26:41 +0200

    emacs / pdf-tools: deleted obsolete sk:fit-to-page commands

--- .emacs.d/config.d/84--pdftools.el
+++ .emacs.d/config.d/84--pdftools.el
@@ -5,6 +5,7 @@
 (use-package pdf-tools
   :mode ("\\.pdf\\'" . pdf-view-mode)
   :config
+  (setq-default pdf-view-display-size 'fit-page)
   (setq pdf-view-resize-factor 1.1)
   (setq pdf-annot-activate-created-annotations t)
   (setq pdf-view-midnight-invert nil)
@@ -15,29 +16,6 @@
   (add-hook 'pdf-view-mode-hook #'pdf-annot-minor-mode)
   (add-hook 'pdf-view-mode-hook #'pdf-isearch-minor-mode)
   (add-hook 'pdf-view-mode-hook #'pdf-outline-minor-mode)
-  (add-hook 'pdf-view-mode-hook #'sk:pdf-view-auto-fit-to-window-minor-mode)
-
-
-
-  (defun sk:pdf-view-fit-to-window ()
-    "fits pdf into window (automatically detect if fit-height or fit-width should be used)"
-    (interactive)
-    (let* ((pdf-width (car (pdf-view-image-size t)))
-           (pdf-height (cdr (pdf-view-image-size t)))
-           (pdf-aspect-ratio (/ (* 1.0 pdf-width) pdf-height))
-           (window-width (window-pixel-width))
-           (window-height (window-pixel-height))
-           (window-aspect-ratio (/ (* 1.0 window-width) window-height)))
-      (if (> pdf-aspect-ratio window-aspect-ratio)
-          (pdf-view-fit-width-to-window)
-        (pdf-view-fit-height-to-window))))
-
-  (define-minor-mode sk:pdf-view-auto-fit-to-window-minor-mode
-    "automatically fit viewed pages to the current window, so that the whole page is always visible"
-    :lighter " "
-    (if sk:pdf-view-auto-fit-to-window-minor-mode
-        (add-hook 'pdf-view-after-change-page-hook #'sk:pdf-view-fit-to-window nil 'local)
-      (remove-hook 'pdf-view-after-change-page-hook #'sk:pdf-view-fit-to-window 'local)))
 
 
 
@@ -54,10 +32,8 @@
     "K" 'pdf-view-previous-page
     "M" 'pdf-view-goto-page
 
-    "=" 'sk:pdf-view-fit-to-window
     "C-c g" #'(lambda () (interactive) (pdf-view-redisplay t))
     "C-c C-r s" 'pdf-view-auto-slice-minor-mode
-    "C-C C-r z" 'sk:pdf-view-auto-fit-to-window-minor-mode
     "C-c C-a d" 'pdf-annot-delete))
 
 
@@ -72,6 +48,7 @@
   "< b" 'image-bob
   "> b" 'image-eob
 
+  "P" 'image-transform-fit-to-window
   "W" 'image-transform-fit-to-width
   "H" 'image-transform-fit-to-height
 
