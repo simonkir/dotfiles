dcb5ba7eb1758967c4ed3787f03548ed975a16dc
simonkir <simon.kugelblitz@gmail.com>
2023-10-03 23:10:16 +0200

    conky: adjusted layout slightly

--- .config/conky/launch-conky.sh
+++ .config/conky/launch-conky.sh
@@ -7,7 +7,7 @@ killall conky
 if [[ $connected_monitors == 2 ]]; then
     # screen 1 -- primary
     conky --config=$HOME/.config/conky/clock.conkyrc --alignment tm -y 100 &
-    conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bm -y 70 &
+    conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bm -y 75 &
 
     # screen 2 -- secondary
     conky --config=$HOME/.config/conky/clock.conkyrc --alignment tl -x -1820 &
@@ -15,6 +15,6 @@ if [[ $connected_monitors == 2 ]]; then
 
 elif [[ $connected_monitors == 1 ]]; then
     # screen 1 -- primary
-    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tm -y 80 &
+    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tm -y 55 &
     conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bm -y 70 &
 fi
