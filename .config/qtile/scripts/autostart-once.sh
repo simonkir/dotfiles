fc3203cfdc3ddbdd3a75ae39cb90547dbb0d0d85
simonkir <simon.kugelblitz@gmail.com>
2023-12-28 22:06:58 +0100

    minor adjustments

--- .config/qtile/scripts/autostart-once.sh
+++ .config/qtile/scripts/autostart-once.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-sxhkd -c ~/.config/sxhkd/sxhkdrc &
 
-volumeicon &
-nm-applet &
-blueberry-tray &
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 xfce4-power-manager &
 
-~/.config/picom/picom.sh &
+
+
 ~/.emacs.d/launch-daemon.sh &
+sxhkd -c ~/.config/sxhkd/sxhkdrc &
+
+
+
+~/.config/picom/picom.sh &
 ~/.config/simonkir/random-wallpaper.sh &
 ~/.config/conky/launch-conky.sh &
+
+volumeicon &
+nm-applet &
+
+if ! bluetoothctl show | grep -q "No default controller available"; then
+    blueberry-tray &
+fi
