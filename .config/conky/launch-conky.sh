5a160aa440b5db2b9761c54123066bbc34e19262
simonkir <simon.kugelblitz@gmail.com>
2024-04-19 13:29:44 +0200

    conky: slight layout change

--- .config/conky/launch-conky.sh
+++ .config/conky/launch-conky.sh
@@ -14,7 +14,6 @@ if test $screencount = 2
     conky --xinerama-head=1 --config=$HOME/.config/conky/quotes.conkyrc --alignment bl -y 70 -x 50 &
 
 else if test $screencount = 1
-    # screen 1 -- primary
-    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tm -y 55 &
-    conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bm -y 70 &
+    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tm -y 85 &
+    conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bm -y 85 &
 end
