5472a4ce0f25a5a1a83679a3c459014d026bec2e
simonkir <simon.kugelblitz@gmail.com>
2024-05-08 08:59:58 +0200

    small changes (qtile icons, emacs / eglot)

--- .emacs.d/config.d/81--lsp.el
+++ .emacs.d/config.d/81--lsp.el
@@ -5,6 +5,7 @@
   :hook (python-mode . eglot-ensure)
   :config
   (setq eglot-autoshutdown t)
+  (setq eglot-events-buffer-size 0)
   (setq eglot-ignored-server-capabilities '(:hoverProvider)))
 
 ; * xref
