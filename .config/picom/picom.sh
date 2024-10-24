25868f71984f6dd5ab2be645484c154d5439b12e
simonkir <simon.kugelblitz@gmail.com>
2024-10-24 09:30:36 +0200

    minor fixes

--- .config/picom/picom.sh
+++ .config/picom/picom.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
 killall picom
-picom --config ~/.config/picom/picom.conf --window-shader-fg ~/.config/picom/darkmode.glsl &
+picom --config ~/.config/picom/picom.conf --window-shader-fg ~/.config/picom/darkmode.glsl
