17d36200888635f958a5633ad1b7eeeb89b95e52
simonkir <simon.kugelblitz@gmail.com>
2024-12-09 19:13:37 +0100

    small changes (numlock, keybinds)

--- .config/qtile/scripts/autostart-always.sh
+++ .config/qtile/scripts/autostart-always.sh
@@ -6,11 +6,4 @@
 
 # keymap resets after system changes (e. g. system upgrades, xrandr changes)
 setxkbmap -option caps:escape &
-
-# when using laptop w/o external monitor, numpad should be off
-# in order to to access <end>, <home> etc. keys
-if test (xrandr --prop | grep " connected" | wc -l) = 2
-    numlockx on &
-else
-    numlockx off &
-end
+numlockx on &
