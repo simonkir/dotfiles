f9d8820f5c85b8c98d6637a4fe56bc6a9a20bd35
simonkir <simon.kirschenmann@gmail.com>
2025-08-13 20:24:52 +0200

    conky: added worldclock widget

--- .config/conky/launch-conky.sh
+++ .config/conky/launch-conky.sh
@@ -6,14 +6,17 @@ killall conky
 
 if test $screencount = 2
     # screen 1 -- primary
-    conky --xinerama-head=0 --config=$HOME/.config/conky/clock.conkyrc --alignment tm -y 100 &
+    conky --xinerama-head=0 --config=$HOME/.config/conky/clock.conkyrc --alignment tm -x 125 -y 100 &
+    conky --xinerama-head=0 --config=$HOME/.config/conky/worldclock.conkyrc --alignment tm -x -215 -y 100 &
     conky --xinerama-head=0 --config=$HOME/.config/conky/quotes.conkyrc --alignment bm -y 95 &
 
     # screen 2 -- secondary
     conky --xinerama-head=1 --config=$HOME/.config/conky/clock.conkyrc --alignment tl -x 100 &
+    conky --xinerama-head=1 --config=$HOME/.config/conky/worldclock.conkyrc --alignment tr -x 80 &
     conky --xinerama-head=1 --config=$HOME/.config/conky/quotes.conkyrc --alignment bl -y 70 -x 50 &
 
 else if test $screencount = 1
-    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tm -y 85 &
+    conky --config=$HOME/.config/conky/clock.conkyrc --alignment tm -x 125 -y 85 &
+    conky --config=$HOME/.config/conky/worldclock.conkyrc --alignment tm -x -215 -y 85 &
     conky --config=$HOME/.config/conky/quotes.conkyrc --alignment bm -y 85 &
 end
