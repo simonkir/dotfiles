296c47d81c5e545a44d6eb36569d991935a174ab
simonkir <simon.kirschenmann@gmail.com>
2025-08-28 16:19:23 -0400

    qtile: updated group rules

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -105,14 +105,16 @@ group_names = [i for i in "12345678"]
 group_layouts = {i: "max" for i in group_names}
 group_layouts["2"] = "plasma"
 group_layouts["3"] = "plasma"
+group_layouts["4"] = "plasma"
 
 # group name : wm_class (first or second field)
 group_rules = {i: [] for i in group_names}
 group_rules["1"] = ["firefox", "brave-browser", "vivaldi-stable"]
 group_rules["2"] = ["emacs", "emacs-30-1", "libreoffice"]
-group_rules["4"] = ["Alacritty"]
-group_rules["4"] = ["thunderbird", "spotify"]
-group_rules["5"] = ["krita", "Inkscape"]
+group_rules["3"] = ["Alacritty"]
+group_rules["4"] = ["thunderbird"]
+group_rules["5"] = ["spotify"]
+group_rules["6"] = ["krita", "Inkscape"]
 
 for grname in group_names:
     groups.append(Group(name=grname,
