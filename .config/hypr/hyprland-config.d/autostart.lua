dc7390c4784a5eece79973d7afef0225372b0f9e
simikir <simi@kir.org>
2026-09-19 23:08:52 +0200

    hyprland: removed unused autostarts

--- .config/hypr/hyprland-config.d/autostart.lua
+++ .config/hypr/hyprland-config.d/autostart.lua
@@ -1,12 +1,6 @@
 hl.on("hyprland.start", function ()
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
-  hl.exec_cmd("systemctl --user start ssh-agent.service")
 
   hl.exec_cmd("noctalia")
-  -- hl.exec_cmd("~/.config/conky/launch-conky.sh")
-
   hl.exec_cmd("emacs --daemon")
-
-  hl.exec_cmd("wl-past --type text --watch cliphist store")
-  hl.exec_cmd("wl-past --type image --watch cliphist store")
 end)
